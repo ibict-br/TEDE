409534cb6a5dd72725425f8f56b4624977c310c3
Márcio Gurgel <marcio.rga@gmail.com>
2014-06-25 00:44:16 -0300

    [Controle de autoridade] Problema com limite de 100 caracteres. [Importação/Controle de autoridade] NullPointer no método "getBestMatch"

--- dspace-api/src/main/java/org/dspace/content/authority/DCInputAuthority.java
+++ dspace-api/src/main/java/org/dspace/content/authority/DCInputAuthority.java
@@ -7,12 +7,17 @@
  */
 package org.dspace.content.authority;
 
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.log4j.Logger;
@@ -45,17 +50,33 @@ public class DCInputAuthority extends SelfNamedPlugin implements ChoiceAuthority
     private static Logger log = Logger.getLogger(DCInputAuthority.class);
 
     private Map<String, String> valueAndLabel;
-    
-    private String values[] = null;
-    private String labels[] = null;
+    private Map<String, Long> valueAndHash;
+    private Set<Long> usedHashes = new HashSet<Long>();
 
+    private MessageDigest messageDigest;
+    
     private static DCInputsReader dci = null;
     private static String pluginNames[] = null;
 
     public DCInputAuthority()
     {
         super();
+        initMessageDigest();
     }
+
+    /**
+     * Initializes {@link #messageDigest}, using <i>MD5</i> hash
+     */
+	private void initMessageDigest() {
+		try 
+        {
+        	messageDigest = MessageDigest.getInstance("MD5");
+        } 
+        catch (NoSuchAlgorithmException e) 
+        {
+        	log.error(e.getMessage(), e);
+        }
+	}
 
     public static String[] getPluginNames()
     {
@@ -83,10 +104,10 @@ public class DCInputAuthority extends SelfNamedPlugin implements ChoiceAuthority
                 log.error("Failed reading DCInputs initialization: ",e);
             }
             List<String> names = new ArrayList<String>();
-            Iterator pi = dci.getPairsNameIterator();
+            Iterator<String> pi = dci.getPairsNameIterator();
             while (pi.hasNext())
             {
-                names.add((String)pi.next());
+                names.add(pi.next());
             }
 
             pluginNames = names.toArray(new String[names.size()]);
@@ -97,9 +118,10 @@ public class DCInputAuthority extends SelfNamedPlugin implements ChoiceAuthority
     // once-only load of values and labels
     private void init()
     {
-        if (valueAndLabel == null)
+        if (valueAndLabel == null && valueAndHash == null)
         {
         	valueAndLabel = new LinkedHashMap<String, String>();
+        	valueAndHash = new LinkedHashMap<String, Long>();
         	
             String pname = this.getPluginInstanceName();
             List<String> pairs = dci.getPairs(pname);
@@ -108,7 +130,11 @@ public class DCInputAuthority extends SelfNamedPlugin implements ChoiceAuthority
                 for (int i = 0; i < pairs.size(); i += 2)
                 {
                 	/** Map key (value) **/
-                	valueAndLabel.put(pairs.get(i+1), pairs.get(i));
+                	String value = pairs.get(i+1);
+					String label = pairs.get(i);
+					
+					valueAndLabel.put(value, label);
+                	valueAndHash.put(value, generateHashUniqueFromString(value, null));
                 }
                 log.debug("Found pairs for name="+pname);
             }
@@ -120,7 +146,9 @@ public class DCInputAuthority extends SelfNamedPlugin implements ChoiceAuthority
     }
 
 
-    public Choices getMatches(String field, String query, int collection, int start, int limit, String locale)
+  
+
+	public Choices getMatches(String field, String query, int collection, int start, int limit, String locale)
     {
         init();
 
@@ -133,7 +161,7 @@ public class DCInputAuthority extends SelfNamedPlugin implements ChoiceAuthority
         {
         	if (currentValue.getValue().toLowerCase().contains(query.toLowerCase()))
         	{
-        		choices.add(new Choice(currentValue.getKey(), currentValue.getValue(), currentValue.getValue()));
+        		choices.add(new Choice(String.valueOf(valueAndHash.get(currentValue.getValue())), currentValue.getValue(), currentValue.getValue()));
         		dflt = i++;
         	}
         }
@@ -141,17 +169,21 @@ public class DCInputAuthority extends SelfNamedPlugin implements ChoiceAuthority
 		return new Choices(choices.toArray(choicesArray), 0, choices.size(), Choices.CF_AMBIGUOUS, false, dflt);
     }
 
+	
+
     public Choices getBestMatch(String field, String text, int collection, String locale)
     {
         init();
-        for (int i = 0; i < values.length; ++i)
+        
+        for(Map.Entry<String, String> currentValue : valueAndLabel.entrySet())
         {
-            if (text.equalsIgnoreCase(values[i]))
-            {
-                Choice v[] = new Choice[1];
-                v[0] = new Choice(String.valueOf(i), values[i], labels[i]);
-                return new Choices(v, 0, v.length, Choices.CF_UNCERTAIN, false, 0);
-            }
+        	if (text.equalsIgnoreCase(currentValue.getValue()))
+        	{
+        		Choice v[] = new Choice[1];
+        		v[0] = new Choice(String.valueOf(valueAndHash.get(currentValue.getValue())), currentValue.getValue(), currentValue.getValue());
+        		return new Choices(v, 0, v.length, Choices.CF_UNCERTAIN, false, 0);
+        	}
+        	
         }
         return new Choices(Choices.CF_NOTFOUND);
     }
@@ -159,6 +191,48 @@ public class DCInputAuthority extends SelfNamedPlugin implements ChoiceAuthority
     public String getLabel(String field, String key, String locale)
     {
         init();
-        return valueAndLabel.get(key);
+        
+        for(Map.Entry<String, Long> currentIteration : valueAndHash.entrySet())
+        {
+        	if(String.valueOf(currentIteration.getValue()).equals(key))
+        	{
+        		return valueAndLabel.get(currentIteration.getKey());
+        	}
+        }
+        
+        return "";
     }
+    
+    /**
+     * Evicts two or more string different strings to recieve same generated hash
+     * @param originalValue Original value
+     * @param changedValue Changed value, used to distinguish a given String
+     * @return Unique hash
+     */
+    private Long generateHashUniqueFromString(String originalValue, String changedValue) 
+    {
+    	Long generateHashFromString = changedValue != null ? generateHashFromString(changedValue) : generateHashFromString(originalValue);
+    	if(!usedHashes.contains(generateHashFromString))
+    	{
+    		usedHashes.add(generateHashFromString);
+    		valueAndHash.put(originalValue, generateHashFromString);
+    		return generateHashFromString;
+    	}
+    	else
+    	{
+    		/** Change the original String to get a different hash **/
+    		return generateHashUniqueFromString(originalValue, changedValue + "::");
+    	}
+	}
+    
+    /**
+     * Generate a {@link Long} using {@link #messageDigest} plus a String value
+     * @param value
+     * @return Long value (converted to String)
+     */
+    private Long generateHashFromString(String value) 
+	{
+		ByteBuffer byteBuffer = ByteBuffer.wrap(messageDigest.digest(value.getBytes()));
+		return byteBuffer.getLong();
+	}
 }
